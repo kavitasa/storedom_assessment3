bbdf726d978e69cf4706d44cac86c054e6df2b1b
Kavita <okavita@gmail.com>
2014-12-06 16:03:07 -0700

    deactivating items criteria fulfilled

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,16 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    if params[:show_inactive] == "true"
+      @items = Item.all
+    else
+      @items = Item.where(active: true)
+    end
+  end
+
+  def activate
+    item = Item.find(params[:id])
+    item.update(active: true)
+    redirect_to items_path(show_inactive: true)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'items#index'
 
-  resources :items,  only: [:index, :show]
+  resources :items,  only: [:index, :show] do
+    member do
+      post 'activate'
+    end
+  end
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 end
