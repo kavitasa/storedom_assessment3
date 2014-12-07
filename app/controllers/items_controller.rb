259719520b3a5a9a318553b161cb8d25452fc063
Kavita <okavita@gmail.com>
2014-12-07 12:06:14 -0700

    update activate in controller update method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,20 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    if params['show_inactive']
+      @items = Item.all
+    else
+      @items = Item.where(active: true)
+    end
   end
 
   def show
     @item = Item.find(params[:id].to_i)
+  end
+
+  def update
+    # puts params
+    item = Item.find(params[:id].to_i)
+    item.update(active: true)
+    redirect_to items_path
   end
 end
--- test/integration/display_items_test.rb
+++ test/integration/display_items_test.rb
@@ -15,4 +15,45 @@ class DisplaysItemsTest < ActionDispatch::IntegrationTest
     end
   end
 
+  def test_it_shows_active_items
+    Item.create(name: 'silly', description: 'goose goose goose goose goose goose goose goose')
+    visit '/'
+    assert page.has_content?('silly')
+    assert page.has_content?('goose')
+  end
+
+  def test_it_does_not_show_inactive_items
+    Item.create(name: 'silly', description: 'goose goose goose goose goose goose goose goose', active: false)
+    visit '/'
+    refute page.has_content?('silly')
+    refute page.has_content?('goose')
+  end
+
+  def test_slick_users_can_view_active_and_inactive_items
+    Item.create(name: 'active_robot', active: true)
+    Item.create(name: 'inactive_drone', active: false)
+    visit '/?show_inactive=true'
+    assert page.has_content?('active_robot')
+    assert page.has_content?('inactive_drone')
+  end
+
+  def test_only_inactive_items_have_active_button
+    Item.create(name: 'active_robot', active: true)
+    Item.create(name: 'inactive_drone', active: false)
+    visit '/?show_inactive=true'
+    within '.item_name', text: 'active_robot' do
+      refute page.has_button?('Activate')
+    end
+    within '.item_name', text: 'inactive_drone' do
+      assert page.has_button?('Activate')
+    end
+  end
+
+  def test_clicking_activate_sets_inactive_item_to_active
+    Item.create(name: 'inactive_drone', active: false)
+    visit '/?show_inactive=true'
+    click_on('Activate')
+    assert page.has_content?('inactive_drone')
+  end
+
 end
